b76cefed04ab769bf9c4c9aec90b48cdf6131dfe
1205324997 <josi.i.dami@gmail.com>
2023-03-01 04:27:00 -0500

    validacion de tarjeta

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+
 import validator from './validator.js';
 
-console.log(validator);
+let elcardNumber = document.getElementById("cardNumber");
+const elcardValidation = document.getElementById("cardValidator");
+
+const checkCC = () => {  
+    let message = "";
+
+    if (validator.isValid(elcardNumber.value))
+        message = "Número valido";
+    else
+        message = "Número invalido";
+
+    // Inicializar input con resultado
+    elcardValidation.value = message;
+};
+
+// Evento botón submit
+document.getElementById("cardForm").addEventListener("submit", (e) => {
+    e.preventDefault()
+    checkCC()
+    const maskifyNumber = validator.maskify(elcardNumber.value)
+    console.log (maskifyNumber);
+    elcardNumber.value = maskifyNumber;
+    
+});
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,62 @@
 const validator = {
-  // ...
+  isValid: (cardNumber) => {
+    const length = cardNumber.length;
+    let addDigits = 0;
+    if (length % 2 == 0) {
+
+      for (let i = length - 1; i >= 0; i--) 
+      {
+        let currentDigit = parseInt(cardNumber[i]);
+        if (i % 2 == 0) 
+        { currentDigit *= 2;
+          if (currentDigit > 9) 
+          {
+          
+            let firstNumber = parseInt(currentDigit / 10); 
+            let trailingNumber = currentDigit % 10; 
+
+            
+            currentDigit = firstNumber + trailingNumber;
+          }
+        }
+        addDigits += currentDigit; 
+      }
+    }
+    else {
+    
+      for (let i = length - 1; i >= 0; i--) {
+        let currentDigit = parseInt(cardNumber[i]);
+        if ((i - 1) % 2 == 0) 
+        { currentDigit *= 2;
+          if (currentDigit > 9) {
+            
+            let firstNumber = parseInt(currentDigit / 10);
+            let trailingNumber = currentDigit % 10;
+
+            
+            currentDigit = firstNumber + trailingNumber;
+          }
+        }
+
+        addDigits += currentDigit; 
+      }
+    }
+    const resultado = (addDigits % 10) === 0;
+    console.log(resultado, addDigits);
+    return resultado;
+  },
+  maskify: (cardNumber) => {
+    let myArray = []
+    for (let i = 0; i < cardNumber.length; i++) {
+      if (i < cardNumber.length - 4) {
+        myArray.push("#");
+      } else {
+        myArray.push(cardNumber[i]);
+      }
+    }
+    return myArray.join("");
+  }
 };
 
 export default validator;
+
