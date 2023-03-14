5f0712e77c8992b975f4b15ec393d8722f52f234
1205324997 <josi.i.dami@gmail.com>
2023-03-14 01:04:21 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
 import validator from "./validator.js";
 
-const number = document.getElementById("number");
+// elementos del DOM
+const number = document.getElementById("number");//llamamos la Id por su nombre
 const result = document.getElementById("result");
-const btn = document.getElementById("btnPay");
-
-function printValidation() {
-  const cardNumber = number.value;
-  const isValid = validator.isValid(cardNumber);
+const btn = document.getElementById("btnEnviar");
+//realizamos la validacion del numero ingresado
+function validarNumero() {
+  const cardNumber = number.value;//creamos una constante con el valor del numero y la evaluamos
+  const isValid = validator.isValid(cardNumber);//creamos una cosntante para saber si es valido o no el numero ingresado
   if (isValid) {
-    result.innerHTML = "Tarjeta válida";
+    result.innerHTML = "Tarjeta válida";// Si al enviar el evento el resultado es valido nos bota una mensaje tarjeta valida 
   } else {
-    result.innerHTML = "Tarjeta inválida";btn
+    result.innerHTML = "Tarjeta inválida";//Si al enviar el evento el resultado es valido nos bota una mensaje tarjeta invalida
   }
-
-  const coverNumber = validator.maskify(cardNumber, "#", 4);
+  const coverNumber = validator.maskify(cardNumber, "#", 4);//
   number.value = coverNumber;
 }
 
-btn.addEventListener("click", printValidation);
-const numberr = document.getElementById("number");
-numberr.addEventListener("keyup", (event) => {
-  const num = event.target.value;
-  event.target.value = num.replace(/\s/g, "").replace(/\D/g,"");
+//Capturamos un evento
+btn.addEventListener("click", validarNumero);
+const numberr = document.getElementById("number");//creamos una variable con el valor de Id
+numberr.addEventListener("keyup", (event) => {//capturamos nuevamente el evento con el valor de la variable declarada y damos una funcion
+  const numTarjeta = event.target.value;//Evaluamos el resultado de el evento de la linea (propiedad)
+  event.target.value = numTarjeta.replace(/\D/g, "").replace(/\s/g, "");//reemplazmos los caracteres si nos dan valores que no sean numericos
 });
--- src/validator.js
+++ src/validator.js
@@ -1,46 +1,45 @@
 const validator = {
-  isValid: function (num) {
-    const cardNumberArray = Array.from(String(num), Number);
-    let positionSum = 0;
+  isValid: function(numTarjeta) {
+    {
+      //retornamos los numeros en forma de funcion los invertimos y separamos
+      const reversar = numTarjeta.toString().split("").reverse(); 
+      let total = 0; //realiza la suma de los digitos 
 
-    for (let i = cardNumberArray.length - 1; i >= 0; i--) {
-      let multiplication = 0;
-
-      if (i % 2 === 0) {
-        multiplication = cardNumberArray[i] * 2;
-        if (multiplication >= 10) {
-          let remainder = 0;
-          let sumOfDigits = 0;
-
-          while (multiplication) {
-            remainder = multiplication % 10;
-            sumOfDigits = sumOfDigits + remainder;
-            multiplication = Math.floor(multiplication / 10);
+    
+      //damos al indice el valor de 0, para buscar los numeros pares si fuera impares comensariamos desde 1
+      for (let i = 0; i < reversar.length; i++) {  
+        let numeSuma = parseInt(reversar[i]); 
+        //verificamos los numeros, para multiplicar los que se encuntran en pocision impar por 2
+        if (i % 2 === 1) { 
+          numeSuma *= 2; 
+          if (numeSuma > 9) {
+            numeSuma -= 9;
           }
-
-          positionSum += sumOfDigits;
-        } else {
-          positionSum += multiplication;
         }
-      } else {
-        positionSum += cardNumberArray[i];
+        total += numeSuma; //concatenamos el valor de la pocision con el resultado de numeSuma
       }
-    }
-
-    const final = positionSum % 10;
-
-    if (final === 0) {
-      return true;
-    } else {
-      return false;
+      if (total % 10 === 0) { // verificar si el valor es divisible para 10
+        return true; // si el resultado es correcto nos devuelve true
+      } else {
+        return false; // si el resultado es incorrecto nos devuelve false
+      }
     }
   },
 
-  maskify: function (string, mask, numberOfDigits) {
-    return (
-      ("" + string).slice(0, -numberOfDigits).replace(/./g, mask) +
-      ("" + string).slice(-numberOfDigits)
-    );
+  /*maskify: function (numTarjeta) {
+    return numTarjeta.replace(/.(?=.{4})/g, "#")*///reemplazmos todo con expresiones regulares
+
+  maskify: function (numTarjeta) {//creamos una funcion con el vamor de isvalid
+    const convirtiendo = numTarjeta.split("");//dividimos el string en una array
+    let result = ''; 
+    for (let i = 0; i < numTarjeta.length - 4; i++) {//Medimos la longitud del numero ingresado menos los 4 ultimos para enmmascarar
+      convirtiendo[i] = "#";//convertimos los digitos en numeral menos los 4 ultimos
+    }
+    for (let i = 0; i < convirtiendo.length; i ++) {
+      result = result + convirtiendo[i];
+    }
+   
+    return result  
   },
 };
 
